3ba714969a586643b10ae5d9c7653516e283722e
pgt-2020-07-dimas <dimas.stmp@gmail.com>
2020-03-16 12:52:16 +0700

    Versi 2

--- penjumlahan.php
+++ penjumlahan.php
@@ -15,8 +15,8 @@ echo "Gagal melakukan koneksi ke MySQL: " . mysqli_connect_error();
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 </head>
-<body class="container-fluid shadow-lg p-3 mb-5 bg-white rounded bg-light"> 
-	<div class="-fluid">
+<body class="container-sm shadow-lg p-3 mb-5 bg-white rounded bg-light"> 
+	<div class="container">
 		<h2>Penjumlahan Beruntun</h2> 
 	</div>
 	<form class="container" name="form-1" method="POST">
